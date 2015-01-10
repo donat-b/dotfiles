1bad1893d886066defd2a37b5c73c03ca61e7808
donat_b <donat@opmbx.org>
2015-01-10 13:13:24 +0300

    Update ranger

--- install.sh
+++ install.sh
@@ -1,18 +1,47 @@
 #!/usr/bin/env sh
-# Istall symlinks
+#
+# Make links for configs in home directory
+#
+# donat@opmbx.org
+# 2014-12-25
+
+DOTDIR=$HOME/dotfiles
 
 function sl() {
-	# make a relative symlink in $HOME directory
 	# <name> <config name>
-	[ ! -f "$HOME/$2" ] && \
-		ln -sr "$1" "$HOME/$2"
+	if [ -h "$HOME/$2" ]; then
+		printf 'warn: %s exists - symlink\n' "$1"
+	elif [ -e "$HOME/$2" ]; then
+		printf 'warn: %s exists -%s\n' "$1" "$(file "$HOME/$2" | cut -d: -f2)"
+	else
+		ln -sv "$DOTDIR/$1" "$HOME/$2"
+	fi
 }
 
-cd ~/dotfiles
+# git
+sl gitconfig .gitconfig
+
+# mpv
+sl mpv.conf .config/mpv/mpv.conf
+
+# mutt
+sl muttrc .muttrc
+
+# ranger
+sl rangerrc .config/ranger/rc.conf
 
 # tmux
-sl tmux.conf .tmux.conf
+sl tmux/conf .tmux.conf
+
 # vim
 sl vimrc .vimrc
+
+# x11 stuff
+sl i3config .i3/config
+sl xinitrc .xinitrc
+sl Xresources .Xresources
+
 # zsh
 sl zsh/env .zshenv
+sl zsh/rc .zshrc
+sl zsh/profile .zprofile
