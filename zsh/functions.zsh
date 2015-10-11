fc6f818e6d623d7b26bdafe3882b5f2b4472b290
donat-b <donat@opmbx.org>
2015-10-11 12:33:29 +0300

    zsh: add git current_branch function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -3,3 +3,12 @@ function rmwd {
   cd ..
   rm -rvf -- "$d"
 }
+
+#######################################
+#                  GIT
+#######################################
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
