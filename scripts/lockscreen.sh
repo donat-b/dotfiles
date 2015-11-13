0bd0eb3b948920462d3cfb3163933494b1736e69
donat-b <donat@opmbx.org>
2015-11-13 12:40:48 +0300

    scripts: optimize and improve lockscreen - dropped scrot - faster overlay method - png instead of jpg (needed alpha channel)

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env sh
 # Locks screen with i3lock using DXHR themed background
-# req: convert, scrot, i3lock
+# req: imagemagick, i3lock
 # 2015-11-15
 
 declare -r lockscreen_bg=/tmp/ls_bg.png
+declare -r overlay="$HOME/dotfiles/media/dx_triangles.png"
+declare -r i3lock_cmd='i3lock --nofork -c 000000 --ignore-empty-password --show-failed-attempts'
+declare -r c_blur=4x3
+declare -r c_cmp_method=linearlight
 
 function genbg {
-  scrot "$lockscreen_bg"
-  convert "$lockscreen_bg" -gaussian-blur 4x4 "$HOME/dotfiles/media/dx_triangles.jpg" -compose hardlight -composite "$lockscreen_bg"
+  import -window root -quality 70 -define jpeg:dct-method=fastest -compress none jpeg:- \
+    | convert - -blur $c_blur "$overlay" -compose $c_cmp_method -composite "$lockscreen_bg"
 }
 
+#xset dpms force suspend
 killall -SIGUSR1 dunst&
 
 if genbg; then
-  i3lock --nofork -d -i "$lockscreen_bg"
+  $i3lock_cmd --image="$lockscreen_bg"
 else
   printf 'lockscreen: unable to generate background\n'
-  i3lock --nofork -d -c 000000
+  $i3lock_cmd
 fi
 
 killall -SIGUSR2 dunst
