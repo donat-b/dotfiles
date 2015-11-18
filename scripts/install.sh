48c1ff1d61383dc386b30d2457dca2a956954ae3
donat-b <donat@opmbx.org>
2015-11-18 19:26:34 +0300

    i3: restructurize scripts: install: auto fix broken symlinks

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,14 +7,19 @@
 
 DOTDIR=$HOME/dotfiles
 
+function link() {
+  ln -sv --force "$1" "$2"
+}
+
 function sl() {
 	# <path to config in dotfiles> <path to config in $HOME>
-	if [ -h "$HOME/$2" ]; then
-		printf 'warn: %s exists - symlink\n' "$1"
+	if [ -h "$HOME/$2" ] && [ ! -e "$HOME/$2" ]; then
+		printf 'warn: %s exists - a broken symlink\n' "$1"
+		link "$DOTDIR/$1" "$HOME/$2"
 	elif [ -e "$HOME/$2" ]; then
 		printf 'warn: %s exists -%s\n' "$1" "$(file "$HOME/$2" | cut -d: -f2)"
 	else
-		ln -sv "$DOTDIR/$1" "$HOME/$2"
+		link "$DOTDIR/$1" "$HOME/$2"
 	fi
 }
 
@@ -39,8 +44,8 @@ sl tmux/conf .tmux.conf
 sl vim/vimrc .vimrc
 
 # x11 stuff
-sl i3config .i3/config
-sl i3status .config/i3status/config
+sl i3/config .i3/config
+sl i3/i3status .config/i3status/config
 sl X/xinitrc .xinitrc
 sl X/Xresources .Xresources
 
