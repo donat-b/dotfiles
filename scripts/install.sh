be9761a8e7c9d6ca00791c179df1cfc33a994c03
donat-b <donat@opmbx.org>
2015-11-27 13:05:47 +0300

    scripts: a little bit less messy install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,57 +5,51 @@
 # donat@opmbx.org
 # 2014-12-25
 
-DOTDIR=$HOME/dotfiles
+declare -r DOTDIR=$HOME/dotfiles
+declare -A config_paths
 
 function link() {
   ln -sv --force "$1" "$2"
 }
 
 function sl() {
-	# <path to config in dotfiles> <path to config in $HOME>
-	if [ -h "$HOME/$2" ] && [ ! -e "$HOME/$2" ]; then
-		printf 'warn: %s exists - a broken symlink\n' "$1"
-		link "$DOTDIR/$1" "$HOME/$2"
-	elif [ -e "$HOME/$2" ]; then
-		printf 'warn: %s exists -%s\n' "$1" "$(file "$HOME/$2" | cut -d: -f2)"
-	else
-		link "$DOTDIR/$1" "$HOME/$2"
-	fi
+  # <path to config in dotfiles> <path to config in $HOME>
+  if [ -h "$HOME/$2" ] && [ ! -e "$HOME/$2" ]; then
+    printf 'warn: %s exists - a broken symlink\n' "$1"
+    link "$DOTDIR/$1" "$HOME/$2"
+  elif [ -e "$HOME/$2" ]; then
+    # TODO:
+    # decide what is the appropriate action in the case
+    printf 'warn: %s exists -%s\n' "$1" "$(file "$HOME/$2" | cut -d: -f2)"
+  else
+    link "$DOTDIR/$1" "$HOME/$2"
+  fi
 }
 
-# git
-sl gitconfig .gitconfig
+config_paths=(
+# [source]=destination
+# destination can be a dot if a source has the same name
+  [gimp/rc]=.gimp-2.8/gimprc
+  [gitconfig]=.
+  [mpv/mpv.conf]=.config/mpv/mpv.conf
+  [mpv/input.conf]=.config/mpv/input.conf
+  [muttrc]=.
+  [ranger/rc]=.config/ranger/rc.conf
+  [ranger/rifle.conf]=.config/ranger/rifle.conf
+  [tmux/conf]=.tmux.conf
+  [vim/vimrc]=.vimrc
+  [i3/config]=.i3/config
+  [i3/i3status]=.config/i3status/config
+  [X/xinitrc]=.xinitrc
+  [X/Xresources]=.Xresources
+  [zsh/env]=.zshenv
+  [zsh/rc]=.zshrc
+  [zsh/profile]=.zprofile
+  [weechat/alias.conf]=.weechat/alias.conf
+  )
 
-# mpv
-sl mpv/mpv.conf .config/mpv/mpv.conf
-sl mpv/input.conf .config/mpv/input.conf
-
-# mutt
-sl muttrc .muttrc
-
-# ranger
-sl ranger/rc .config/ranger/rc.conf
-sl ranger/rifle.conf .config/ranger/rifle.conf
-
-# tmux
-sl tmux/conf .tmux.conf
-
-# vim
-sl vim/vimrc .vimrc
-
-# x11 stuff
-sl i3/config .i3/config
-sl i3/i3status .config/i3status/config
-sl X/xinitrc .xinitrc
-sl X/Xresources .Xresources
-
-# zsh
-sl zsh/env .zshenv
-sl zsh/rc .zshrc
-sl zsh/profile .zprofile
-
-# gimp
-sl gimp/rc .gimp-2.8/gimprc
-
-# weechat
-sl weechat/alias.conf .weechat/alias.conf
+for i in "${!config_paths[@]}"; do
+  [ "${config_paths[$i]}" == '.' ] && \
+    config_paths[$i]=".$i"
+  sl "$i" "${config_paths[$i]}"
+done
