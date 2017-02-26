47c788487c13adf89860161d4b07c9e36f2cfe33
donat-b <donat@opmbx.org>
2017-02-26 11:31:37 +0300

    update monky.hs

--- monky.hs
+++ monky.hs
@@ -1,12 +1,14 @@
 import Monky
 import Monky.Modules
 
-import Monky.Examples.Alsa
+import Monky.Examples.Battery
 import Monky.Examples.CPU
+import Monky.Examples.Disk
 import Monky.Examples.Memory
 import Monky.Examples.Network
+import Monky.Examples.Sound.Alsa
 import Monky.Examples.Time
-import Monky.Examples.Disk
+import Monky.Examples.Wifi
 
 import Monky.Outputs.I3
 
@@ -20,12 +22,13 @@ filterNs str = not ("tun" `isPrefixOf` str)
 main :: IO()
 main = startLoop (getI3Output)
   [ pollPack 1 $ getDynamicHandle "No Network" filterNs
+  -- breaks i3bar
+  {-, evtPack    $ getWifiHandle [FormatName, FormatText ":", FormatChannel] "None" "wlan"-}
   , evtPack    $ getVOLHandle "default"
   , pollPack 5 $ getCPUHandle ScalingCur "thermal_zone0"
   , pollPack 3 $ getMemoryHandle
-  , pollPack 10 $ getDiskHandle "f9546c54-8df5-4572-a868-ef3cbb0d9ec5"
-  , pollPack 10 $ getDiskHandle "e3cc9481-7566-4a2f-8fcd-54181fc0842b"
-  , pollPack 10 $ getDiskHandle "22501ea5-3289-4a5f-bcda-c0d6edc26713"
+  {-, pollPack 9 $ getDiskHandle "80f3cf1d-36f1-47cc-bd2a-c3ee0b1299dd"-}
+  , pollPack 2 $ getBatteryHandle' "AC0"
   , pollPack 1 $ getTimeHandle "%a %d %b %H:%M:%S"
   ]
 
